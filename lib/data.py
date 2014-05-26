e4b3fa72f24d2bedb3041bf0cfe6c84caf84384f
Zahan Malkani <zahanm@gmail.com>
2014-05-26 16:49:07 -0700

    Actually generate exposures and conversions

--- lib/data.py
+++ lib/data.py
@@ -1,13 +1,14 @@
 
 from datetime import datetime, timedelta
 import os.path
-from random import randint, choice
+from random import randint, choice, random
 import sqlite3
 
 import names
 
 NUM_PEOPLE = 100
 YOUNGEST_AGE = 10
+MID_LIFE = 20
 OLDEST_AGE = 30
 NUMBER_OF_HITS = 20
 DATA_DIR = '../data'
@@ -27,35 +28,101 @@ class Simulator:
   Simulates a webservice receiving requests and logging each one
   """
 
-  def __init__(self, db):
+  def __init__(self, db: sqlite3.Connection):
     # initialize this service 5 days ago
     self.current_time = datetime.now() - timedelta(days=5)
     self.db = db
     # format: (uid, age, gender)
     self.people = []
     with self.db:
-      self.db.execute('drop table logs')
-      self.db.execute('create table logs (ts timestamp, uid integer)')
+      self.db.execute('drop table if exists exposures')
+      self.db.execute('create table exposures (ts timestamp, uid integer, test1 integer, test2 integer)')
+      self.db.execute('drop table if exists conversions')
+      self.db.execute('create table conversions (ts timestamp, uid integer, conversion text)')
       self.people = list(self.db.execute('select uid, age, gender from people'))
     assert NUM_PEOPLE == len(self.people)
-    self.transactions = []
+    self.exposures = []
+    self.conversions = []
 
   def serverhit(self):
     secs_elapsed = randint(0, 10)
     self.current_time += timedelta(seconds=secs_elapsed)
     person = choice(self.people)
     uid = person[0]
-    self.transactions.append((self.current_time, uid))
+    age = person[1]
+    gender = person[2]
+    test1, test2 = self.expose(uid)
+    convs = self.convert(test1, test2, age, gender)
+    self.exposures.append((self.current_time, uid, int(test1), int(test2)))
+    for conv in convs:
+      self.conversions.append((self.current_time, uid, conv))
+
+  def expose(self, uid: int) -> (bool, bool):
+    # usually we'd calculate a hash ID for the group mapping, but in this toy example, UID - 1 gives a number in [0, 99)
+    hash_key = uid - 1
+    test1 = hash_key < 20
+    test2 = 10 <= hash_key and hash_key < 30
+    return test1, test2
+
+  def convert(self, test1: bool, test2: bool, age: int, gender: str):
+    convs = []
+    if self.convert_a(test1, test2, age, gender):
+      convs.append('A')
+    if self.convert_b(test1, test2, age, gender):
+      convs.append('B')
+    if self.convert_c(test1, test2, age, gender):
+      convs.append('C')
+    return convs
+
+  def convert_a(self, test1: bool, test2: bool, age: int, gender: str):
+    pA = 0.3
+    testdelta = 0
+    if test2:
+      testdelta = 0.02 * pA
+    pA += testdelta
+    return random() < pA
+
+  def convert_b(self, test1: bool, test2: bool, age: int, gender: str):
+    testdelta = 0
+    if age < MID_LIFE:
+      if gender == 'm':
+        pB = 0.2
+      else:
+        pB = 0.15
+    else:
+      if gender == 'm':
+        pB = 0.1
+      else:
+        pB = 0.05
+    if test1:
+      testdelta = 0.1 * pB
+    pB += testdelta
+    return random() < pB
+
+  def convert_c(self, test1: bool, test2: bool, age: int, gender: str):
+    pC = 0.05
+    testdelta = 0
+    if test1:
+      if gender == 'm':
+        testdelta = 0.02 * pC
+      else:
+        testdelta = -0.01 * pC
+    if test2:
+      testdelta = -0.02 * pC
+    pC += testdelta
+    return random() < pC
 
   def flushlogs(self):
     with self.db:
-      self.db.executemany('insert into logs values (?, ?)', self.transactions)
+      self.db.executemany('insert into exposures values (?, ?, ?, ?)', self.exposures)
+      self.db.executemany('insert into conversions values (?, ?, ?)', self.conversions)
 
 class People:
 
-  def __init__(self, db):
+  def __init__(self, db: sqlite3.Connection):
     self.db = db
     with self.db:
+      self.db.execute('drop table people')
       self.db.execute('create table people (uid integer primary key, name text, age integer, gender text)')
 
   def filltable(self):
@@ -69,7 +136,7 @@ class People:
       range(1, NUM_PEOPLE + 1)
     )
     with self.db:
-      self.db.executemany('insert into people values (?, ?, ?)', peeps)
+      self.db.executemany('insert into people values (?, ?, ?, ?)', peeps)
 
 if __name__ == '__main__':
   run()
