8a3081f0c30b7d5b0caf7b2cb31358626e39d626
Zahan Malkani <zahanm@gmail.com>
2014-05-26 11:36:27 -0700

    Learn to use fetchone for COUNT(*) queries

--- lib/tools.py
+++ lib/tools.py
@@ -8,8 +8,8 @@ DB_FILE = 'site.db'
 def countlines():
   conn = sqlite3.connect(os.path.join(DATA_DIR, DB_FILE))
   with conn:
-    for res in conn.execute('select count(*) from logs'):
-      print('{} rows in file'.format(res[0]))
+    res = conn.execute('select count(*) from logs').fetchone()
+    print('{} rows in file'.format(res[0]))
 
 if __name__ == '__main__':
   countlines()
